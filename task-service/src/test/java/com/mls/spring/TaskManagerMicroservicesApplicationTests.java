72818f1568011774a9b06e3ab80508e822563f77
Reisorz <166374263+Reisorz@users.noreply.github.com>
2025-02-03 13:03:56 +0100

    Initial commit

---
+++ task-service/src/test/java/com/mls/spring/TaskManagerMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mls.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
